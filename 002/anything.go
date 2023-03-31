1939f430f8ebafd7f5f25d4d65f9b568b8773a9e
kkinter <ifol1129@gmail.com>
2023-04-01 07:45:19 +0900

    you're doing great

--- 002/anything.go
+++ 002/anything.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	n, err := fmt.Println("Hello World", 23, true)
+	x, _ := fmt.Println("Hello World", 23, true)
+	fmt.Println(n, err)
+	fmt.Println(x)
 	foo()
-	fmt.Println("something more")
+	fmt.Println("something more", 42, true)
 	for i := 0; i < 10; i++ {
 		if i%2 == 0 {
 			fmt.Println(i)
